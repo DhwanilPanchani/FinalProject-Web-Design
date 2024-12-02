04b62765d9017a07a5d75ee33b478989a598dbec
DhwanilPanchani <panchani.d@northeastern.edu>
2024-12-01 20:38:15 -0500

    Freelancer Pages

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 const Job = require('../models/Job');
+=======
+const Job = require('./models/Job');
+>>>>>>> 9052aa3d6 (Freelancer Pages)
 
 // Get All Jobs
 const getAllJobs = async (req, res) => {
